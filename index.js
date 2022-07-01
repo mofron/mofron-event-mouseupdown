6d776b58d40cee7672279028e08a9304b816782e
EC2 Default User <ec2-user@ip-172-31-21-249.ap-northeast-1.compute.internal>
2022-07-01 02:19:15 +0000

    first

---
+++ index.js
@@ -0,0 +1,63 @@
+/**
+ * @file mofron-event-mouseupdown/index.js
+ * @brief mouseup/mousedown event for mofron
+ *        this event notifies when the mouse leave on the component.
+ * ## event function parameter
+ *  - component: event target component object
+ *  - event: "mouseover" event object by addEventListener
+ *  - mixed: user specified parameter
+ * @license MIT
+ */
+
+const MsUp   = require('mofron-event-mouseup');
+const MsDown = require('mofron-event-mousedown');
+
+module.exports = class extends mofron.class.Event {
+    /**
+     * initialize event
+     * 
+     * @param (mixed) short-form parameter
+     *                key-value: event config
+     * @type private
+     */
+    constructor (prm) {
+        try {
+            super();
+            this.modname('MouseUpDown');
+	    if (undefined !== prm) {
+                this.config(prm);
+	    }
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * event contents
+     * 
+     * @param (mofron.class.dom) target dom object
+     * @type private
+     */
+    contents (tgt_dom) {
+        try {
+            let updown = this;
+            let evt = (p1,p2,p3) => {
+                try {
+                    updown.execListener(p3);
+                } catch (e) {
+                    console.error(e.stack);
+                    throw e;
+                }
+            }
+            this.component().event([
+                new MsUp(new ConfArg(evt,true)),
+                new MsDown(new ConfArg(evt, false))
+            ]);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+}
+/* end of file */
